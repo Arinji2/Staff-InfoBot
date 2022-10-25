f3a1aa171c0445a4308a8d48d75ede225cc0030c
Arinji2 <87296817+Arinji2@users.noreply.github.com>
2022-10-25 08:24:09 +0530

    Fix Bugs and Add conditional Rendering

--- src/pages/programs/creator.js
+++ src/pages/programs/creator.js
@@ -1,6 +1,6 @@
 import "../pages.css";
 
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import {
   explainBoilerPlate,
   headBoilerPlate,
@@ -12,12 +12,34 @@ export default function Creator() {
   const [explainItems] = useState([]);
   const [logicItems] = useState([]);
   const [codeItems] = useState([]);
+  const [mainItems] = useState([]);
+  const [clicked, setClicked] = useState(false);
+
+  useEffect(() => {
+    if (clicked === true) {
+      let parent = document.getElementById("codeBox");
+
+      setTimeout(() => {
+        setClicked(false);
+        setTimeout(() => {
+          mainItems.forEach((e) => {
+            let tag = document.createElement("p");
+            let content = document.createTextNode(e);
+            tag.appendChild(content);
+            parent.appendChild(tag);
+          });
+        }, 200);
+      }, 2000);
+    }
+  });
+
   const inputHeading = () => {
     let headTemp = document.getElementById("heading").value;
     setHead(document.getElementById("heading").value);
     let parent = document.getElementById("codeBox");
     headBoilerPlate(headItems, headTemp);
     headItems.forEach((e) => {
+      mainItems.push(e);
       let tag = document.createElement("p");
       let content = document.createTextNode(e);
       tag.appendChild(content);
@@ -30,6 +52,7 @@ export default function Creator() {
     explainItems.push("<h3>" + explain + "</h3>");
     explainBoilerPlate(explainItems, explain, head);
     explainItems.forEach((e) => {
+      mainItems.push(e);
       let tag = document.createElement("p");
       let content = document.createTextNode(e);
       tag.appendChild(content);
@@ -46,6 +69,7 @@ export default function Creator() {
       codeItems.push("<p>" + e.trim() + "</p>");
     });
     codeItems.forEach((e) => {
+      mainItems.push(e);
       let tag = document.createElement("p");
       let content = document.createTextNode(e);
       tag.appendChild(content);
@@ -57,107 +81,110 @@ export default function Creator() {
     let parent = document.getElementById("codeBox");
     logicBoilerPlate(logicItems, logic, head);
     logicItems.forEach((e) => {
+      mainItems.push(e);
       let tag = document.createElement("p");
       let content = document.createTextNode(e);
       tag.appendChild(content);
       parent.appendChild(tag);
     });
   };
-  const copyToClipboard = () => {
-    let textParent = document.getElementById("codeBox");
-    let text = document.getElementById("codeBox").innerText;
-    let textCopy = text;
-    navigator.clipboard.writeText(text);
-    textParent.style.backgroundColor = "green";
-    textParent.style.fontSize = "20px";
-    textParent.style.fontFamily = "Silkscreen";
-    textParent.innerText = "Copied Successfully!";
-    setTimeout(() => {
-      textParent.innerText = textCopy;
-      textParent.style.backgroundColor = "black";
-      textParent.style.fontSize = "14px";
-      textParent.style.lineHeight = "10px";
-      textParent.style.fontFamily = "Open Sans";
-    }, 2000);
+
+  const choiceRender = (option) => {
+    if (option === 1) {
+      return (
+        <div
+          onClick={() => {
+            setClicked(true);
+          }}
+          id="codeBox"
+          className="w-10/12 m-4 content text-sm break-words shadow-xl shadow-white rounded-lg p-4 transition-all ease-in-out duration-500 bg-slate-900 hover:cursor-pointer relative"
+        ></div>
+      );
+    } else
+      return (
+        <div
+          id="codeBox"
+          className="w-10/12 m-4 content text-lg break-words shadow-xl shadow-white rounded-lg p-4 transition-all ease-in-out duration-500 bg-green-600 hover:cursor-pointer relative"
+        >
+          Successfully Copied To clipboard
+        </div>
+      );
   };
   return (
-    <div className="text-center text-3xl text-white flex flex-col items-center justify-center relative">
-      <div className="w-full h-screen hero absolute top-0 -z-10 blur-sm"></div>
-      {/*Heading Section */}
-      <h1 className="text-yellow-400 os font-bold text-2xl md:text-3xl mt-5">
-        Input the Heading
-      </h1>
-      <input
-        placeholder="Heading..."
-        id="heading"
-        className="rounded-md p-1 bg-transparent shadow-lg shadow-black content text-lg text-white outline-none scale-90 md:hover:scale-100 md:focus:scale-100 hover:shadow-md focus:shadow-md hover:shadow-yellow-400 focus:shadow-yellow-400 w-screen md:w-3/12 transition-all ease-in-out duration-500"
-      ></input>
-      <br></br>
-      <button
-        onClick={inputHeading}
-        className=" p-2 rounded-lg text-xl shadow-black shadow-lg hover:scale-90 os bg-slate-600 transition-all ease-out duration-200"
-      >
-        Submit
-      </button>
-      {/*Explanation Section */}
-      <h1 className="text-yellow-400 os font-bold text-2xl md:text-3xl mt-5">
-        Input the Explanation
-      </h1>
-      <input
-        placeholder="Explanation..."
-        id="explanation"
-        className="rounded-md p-1 bg-transparent shadow-lg shadow-black content text-lg text-white outline-none scale-90 md:hover:scale-100 md:focus:scale-100 hover:shadow-md focus:shadow-md hover:shadow-yellow-400 focus:shadow-yellow-400 w-screen md:w-3/12 transition-all ease-in-out duration-500"
-      ></input>
-      <br></br>
-      <button
-        onClick={inputExplanation}
-        className=" p-2 rounded-lg text-xl shadow-black shadow-lg hover:scale-90 os bg-slate-600 transition-all ease-out duration-200"
-      >
-        Submit
-      </button>
-      {/*Code Section */}
-      <h1 className="text-yellow-400 os font-bold text-2xl md:text-3xl mt-5">
-        Input the Code
-      </h1>
-      <input
-        placeholder="Code..."
-        id="code"
-        className="rounded-md p-1 bg-transparent shadow-lg shadow-black content text-lg text-white outline-none scale-90 md:hover:scale-100 md:focus:scale-100 hover:shadow-md focus:shadow-md hover:shadow-yellow-400 focus:shadow-yellow-400 w-screen md:w-3/12 transition-all ease-in-out duration-500"
-      ></input>
-      <br></br>
-      <button
-        onClick={inputCode}
-        className=" p-2 rounded-lg text-xl shadow-black shadow-lg hover:scale-90 os bg-slate-600 transition-all ease-out duration-200"
-      >
-        Submit
-      </button>
-      {/*Logic Section */}
-      <h1 className="text-yellow-400 os font-bold text-2xl md:text-3xl mt-5">
-        Input the Logic
-      </h1>
-      <input
-        placeholder="Logic..."
-        id="logic"
-        className="rounded-md p-1 bg-transparent shadow-lg shadow-black content text-lg text-white outline-none scale-90 md:hover:scale-100 md:focus:scale-100 hover:shadow-md focus:shadow-md hover:shadow-yellow-400 focus:shadow-yellow-400 w-screen md:w-3/12 transition-all ease-in-out duration-500"
-      ></input>
-      <br></br>
-      <button
-        onClick={inputLogic}
-        className=" p-2 rounded-lg text-xl shadow-black shadow-lg hover:scale-90 os bg-slate-600 transition-all ease-out duration-200"
-      >
-        Submit
-      </button>
-      <div
-        onClick={copyToClipboard}
-        id="codeBox"
-        className="w-3/4 content text-sm mt-10 m-5 break-words shadow-xl shadow-black rounded-lg p-4 transition-all ease-in-out duration-500 bg-black hover:cursor-pointer"
-      ></div>
+    <div className="text-center text-3xl text-white flex flex-col items-center justify-center -z-20">
+      <div className="w-full h-fit hero relative">
+        {/*Heading Section */}
+        <h1 className="text-yellow-400 os font-bold text-2xl md:text-3xl mt-5">
+          Input the Heading
+        </h1>
+        <input
+          placeholder="Heading..."
+          id="heading"
+          className="rounded-md p-1 bg-transparent shadow-lg shadow-black content text-lg text-white outline-none scale-90 md:hover:scale-100 md:focus:scale-100 hover:shadow-md focus:shadow-md hover:shadow-yellow-400 focus:shadow-yellow-400 w-screen md:w-3/12 transition-all ease-in-out duration-500"
+        ></input>
+        <br></br>
+        <button
+          onClick={inputHeading}
+          className=" p-2 rounded-lg text-xl shadow-black shadow-lg hover:scale-90 os bg-slate-600 transition-all ease-out duration-200 m-4"
+        >
+          Submit
+        </button>
+        {/*Explanation Section */}
+        <h1 className="text-yellow-400 os font-bold text-2xl md:text-3xl mt-5">
+          Input the Explanation
+        </h1>
+        <input
+          placeholder="Explanation..."
+          id="explanation"
+          className="rounded-md p-1 bg-transparent shadow-lg shadow-black content text-lg text-white outline-none scale-90 md:hover:scale-100 md:focus:scale-100 hover:shadow-md focus:shadow-md hover:shadow-yellow-400 focus:shadow-yellow-400 w-screen md:w-3/12 transition-all ease-in-out duration-500"
+        ></input>
+        <br></br>
+        <button
+          onClick={inputExplanation}
+          className=" p-2 rounded-lg text-xl shadow-black shadow-lg hover:scale-90 os bg-slate-600 transition-all ease-out duration-200 m-4"
+        >
+          Submit
+        </button>
+        {/*Code Section */}
+        <h1 className="text-yellow-400 os font-bold text-2xl md:text-3xl mt-5">
+          Input the Code
+        </h1>
+        <input
+          placeholder="Code..."
+          id="code"
+          className="rounded-md p-1 bg-transparent shadow-lg shadow-black content text-lg text-white outline-none scale-90 md:hover:scale-100 md:focus:scale-100 hover:shadow-md focus:shadow-md hover:shadow-yellow-400 focus:shadow-yellow-400 w-screen md:w-3/12 transition-all ease-in-out duration-500"
+        ></input>
+        <br></br>
+        <button
+          onClick={inputCode}
+          className=" p-2 rounded-lg text-xl shadow-black shadow-lg hover:scale-90 os bg-slate-600 transition-all ease-out duration-200 m-4"
+        >
+          Submit
+        </button>
+        {/*Logic Section */}
+        <h1 className="text-yellow-400 os font-bold text-2xl md:text-3xl mt-5">
+          Input the Logic
+        </h1>
+        <input
+          placeholder="Logic..."
+          id="logic"
+          className="rounded-md p-1 bg-transparent shadow-lg shadow-black content text-lg text-white outline-none scale-90 md:hover:scale-100 md:focus:scale-100 hover:shadow-md focus:shadow-md hover:shadow-yellow-400 focus:shadow-yellow-400 w-screen md:w-3/12 transition-all ease-in-out duration-500"
+        ></input>
+        <br></br>
+        <button
+          onClick={inputLogic}
+          className=" p-2 rounded-lg text-xl shadow-black shadow-lg hover:scale-90 os bg-slate-600 transition-all ease-out duration-200 m-4"
+        >
+          Submit
+        </button>
+      </div>
       {/*Counter Section */}
       <div className="flex flex-row items-center justify-evenly">
         <div className=""></div>
         <div className=""></div>
         <div className=""></div>
       </div>
+      {clicked ? choiceRender(0) : choiceRender(1)}
     </div>
   );
 }
